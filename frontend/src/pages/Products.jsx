bd41832c1513e9c54214a0c49393cc649abb6419
P SHREEKAR SHENOY <shreekarshenoy@gmail.com>
2024-01-13 23:06:44 +0530

    product page updated

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Product from '../components/Product/Product'
 import Footer from '../components/footer/Footer';
+import Carousel from '../components/Carousel/Carousel';
+
+
 
 const products = () => {
   return (
-    <div><Product />
+    <div>
+      <Carousel/>
+      <Product />
     <Footer/></div>
   )
 }
